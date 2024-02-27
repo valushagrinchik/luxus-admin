510bc8ece4dcb94ee448ce19b1139adeff0e2533
Valentina Grinchik <valushagrinchik@gmail.com>
2024-02-27 15:43:23 +0300

    move ws url to env|

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
+export const socket = io(`${process.env.REACT_APP_WS_API}`, {
   autoConnect: false,
   transports: ["websocket", "polling"],
   auth: {
